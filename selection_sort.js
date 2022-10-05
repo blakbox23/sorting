f4bd025347b55bb99c0540c274b3bdde854bca76
blakbox23 <boudii30@gmail.com>
2022-10-05 13:21:36 +0300

    selection_sort implementation

---
+++ selection_sort.js
@@ -0,0 +1,13 @@
+function selectionSort(array) {
+    for(let i=0; i<array.length-1; i++){
+      let min=i;
+      for(let j=i+1; j<array.length; j++){
+        if(array[j] < array[min]){
+        min = j
+        } 
+        [array[i], array[min]] = [array[min], array[i]]
+      }   
+    }
+    return array
+  }
+
